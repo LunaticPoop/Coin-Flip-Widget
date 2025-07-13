e0afb677a0d8d8ff86e33e8409848de1d2dbdee9
LunaticPoop <152500083+LunaticPoop@users.noreply.github.com>
2025-07-13 16:43:30 +0530

    quality of life changes
    just made it look better

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
+        google()
         gradlePluginPortal()
+        mavenCentral()
+    }
+    plugins {
+        id("com.android.application") version "8.4.0"
+        id("org.jetbrains.kotlin.jvm") version "2.0.0" // or your current Kotlin version
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" // ✅ Add this!
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
